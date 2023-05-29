c6e7b6b46cdbbb712aceb4e4c31261a35ab3a0c2
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-05-29 15:06:33 +0530

    update validation

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const path = require("path");
-// const { existsSync, readFileSync } = require("fs");
-// const moduleAlias = require("module-alias");
-
-// if (existsSync(path.join(__dirname, "jsconfig.json"))) {
-//   try {
-//     const jsConfig = JSON.parse(
-//       readFileSync(path.join(__dirname, "jsconfig.json")).toString()
-//     );
-//     const paths = jsConfig.compilerOptions.paths;
-//     const aliases = {};
-//     for (const key in paths) {
-//       if (Object.hasOwnProperty.call(paths, key)) {
-//         const path = paths[key];
-//         const alias = __dirname + "/" + path[0];
-//         aliases[key] = alias;
-//         process.stdout.write("Setting aliaas: " + key + " -> " + alias + "\n");
-//       }
-//     }
-//     moduleAlias.addAliases(aliases);
-//   } catch (e) {
-//     process.stdout.write("Error setting alias" + e + "\n");
-//   }
-// }
-
 process.env.NODE_ENV ||= "development";
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 process.stdout.write(`Using env file .env.${process.env.NODE_ENV}\n`);
